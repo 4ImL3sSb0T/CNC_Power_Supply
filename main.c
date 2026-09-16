3228027725638c938795cc68b9a9cc07648f72bd
4ImL3sSb0T <wansheng06@outlook.com>
2026-09-16 15:41:51 +0800

    添加LED任务以实现LED状态切换，新增FIFO缓冲区实现，支持多线程互斥访问

--- main.c
+++ main.c
@@ -3,9 +3,24 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "driver/led/led.h"
+
+void led_task(void *pvParameters) {
+    (void) pvParameters; // Unused parameter
+
+    led_init(); // Initialize the LED
+
+    while (1) {
+        LED_TOGGLE(); // Toggle the LED state
+        vTaskDelay(pdMS_TO_TICKS(500)); // Delay for 500 milliseconds
+    }
+}
+
 int main()
 {
     stdio_init_all();
+
+    xTaskCreate(led_task, "LED Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     printf("Starting FreeRTOS scheduler...\n");
     vTaskStartScheduler();  
